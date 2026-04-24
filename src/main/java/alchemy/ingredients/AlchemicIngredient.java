0a537614d0b2e923145d43bcf650009e62bdfa31
Turie830 <91200841+Turie830@users.noreply.github.com>
2026-04-24 14:24:46 +0200

    alchemigIngred

--- src/main/java/alchemy/ingredients/AlchemicIngredient.java
+++ src/main/java/alchemy/ingredients/AlchemicIngredient.java
@@ -1,4 +1,4 @@
-package alchemy;
+package alchemy.ingredients;
 
 public class AlchemicIngredient {
     public String name;
@@ -11,17 +11,21 @@ public class AlchemicIngredient {
 
         String[] words = name.split(" ");
 
-        for(int i =0, String word : words; i++) {
-            if (word[i][0])
+        for (String word : words) {
+            if (!Character.isUpperCase(word.charAt(0))
+                    && word.charAt(0) != '\''
+                    && word.charAt(0) != '('
+                    && word.charAt(0) != ')') {
+                return false;
+            }
         }
 
         if (words.length == 1) {
             return words[0].length() >= 3;
         }
-        for(String word : words) {
+        for (String word : words) {
             if (word.length() <= 2) {
                 return false;
             }
-
         }
     }
