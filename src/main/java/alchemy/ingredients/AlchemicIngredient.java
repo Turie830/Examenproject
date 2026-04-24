64eb89bbc2c0edb777334f256b8467c1c33a604f
Turie830 <91200841+Turie830@users.noreply.github.com>
2026-04-24 14:11:03 +0200

    lopololoklolodle

--- src/main/java/alchemy/ingredients/AlchemicIngredient.java
+++ src/main/java/alchemy/ingredients/AlchemicIngredient.java
@@ -1,20 +1,27 @@
-package alchemy.ingredients;
+package alchemy;
 
 public class AlchemicIngredient {
     public String name;
 
 
     public static boolean isValidName(String name) {
-        return (name != null && name.matches("[a-zA-Z'()]+"));
+
+        if (!name.matches("[a-zA-Z'() ]+"))
+            return false;
+
+        String[] words = name.split(" ");
+
+        for(int i =0, String word : words; i++) {
+            if (word[i][0])
+        }
+
+        if (words.length == 1) {
+            return words[0].length() >= 3;
+        }
+        for(String word : words) {
+            if (word.length() <= 2) {
+                return false;
+            }
+
+        }
     }
-
-
-
-
-
-
-
-
-
-
-}
