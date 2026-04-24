0746a3fdfe3115300ce2ec3272ddd3521df6d7c3
Turie830 <91200841+Turie830@users.noreply.github.com>
2026-04-24 13:46:38 +0200

    lopolol

--- src/main/java/alchemy/AlchemicIngredient.java
+++ src/main/java/alchemy/AlchemicIngredient.java
@@ -1,4 +1,20 @@
 package alchemy;
 
 public class AlchemicIngredient {
+    public String name;
+
+
+    public static boolean isValidName(String name) {
+        return (name != null && name.matches("[a-zA-Z'()]+"));
+    }
+
+
+
+
+
+
+
+
+
+
 }
