93fb3bc735cf1afdd05fdb0e48819e4ad9cef200
Obe Willaert <obe.willaert@gmail.com>
2026-04-28 13:55:59 +0200

    State enum

--- src/main/java/alchemy/ingredients/State.java
+++ src/main/java/alchemy/ingredients/State.java
@@ -1,4 +1,13 @@
 package alchemy.ingredients;
 
+/**
+ * An enum for the ingredient types
+ *
+ * @author  Obe Willaert
+ * @author  Arthur Pintelon
+ * @author  Mauro Devolder
+ * @version 1.0
+ */
 public enum State {
+    LIQUID, POWDER
 }
