ea5bb9c0ba68d37643d536efd5cd00ca114d797b
Obe Willaert <obe.willaert@gmail.com>
2026-05-06 18:10:34 +0200

    RecipeBook and RecipeBookTest

--- src/main/java/alchemy/recipes/RecipeBook.java
+++ src/main/java/alchemy/recipes/RecipeBook.java
@@ -1,4 +1,114 @@
 package alchemy.recipes;
 
+import be.kuleuven.cs.som.annotate.Basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A class for recipe books
+ *
+ * @invar TODO geen?
+ *
+ * @author Obe Willaert
+ * @author Mauro Devolder
+ * @author Arthur Pintelon
+ *
+ * @version 1.0
+ */
 public class RecipeBook {
+
+    /**
+     * The list for storing the recipes
+     */
+    private List<Recipe> recipes = new ArrayList<Recipe>();
+
+
+    /**
+     * A constructor to create a new recipe book based on a list of recipes
+     *
+     * @post an empty recipe book is created
+     *      | getNbRecipes() == 0
+     */
+    public RecipeBook() {}
+
+
+    /**
+     * Add a recipe to the book
+     *
+     * @param recipe The recipe to add
+     *
+     * @post The recipe book is longer
+     *      | new.getNbRecipes() == old.getNbRecipes() + 1
+     *
+     * @post The recipe is at the end of the book
+     *      | recipe == new.getRecipeAt(new.getNbRecipes() - 1)
+     *
+     * @throws IllegalArgumentException
+     *      The recipe must be effective
+     *      | recipe == null
+     */
+    public void addRecipe(Recipe recipe) throws IllegalArgumentException {
+        if (recipe == null) {
+            throw new IllegalArgumentException("Recipe cannot be null");
+        }
+
+        recipes.add(recipe);
+    }
+
+    /**
+     * Remove a recipe at an index from the book
+     *
+     * @param index The index of the item to remove
+     *
+     * @post The recipe book contains one recipe less
+     *      | new.getNbRecipes() == old.getNbRecipes() - 1
+     *
+     * @post All recipes after the given index are shifted one position to the left.
+     *      | for each I in index..new.getNbRecipes()-1:
+     *      |     new.getRecipeAt(I) == old.getRecipeAt(I + 1)
+     *
+     * @throws IllegalArgumentException
+     *      The index must be a valid index
+     *      | index < 0 || index >= old.getNbRecipes()
+     */
+    public void removeRecipe(int index) throws IllegalArgumentException {
+        if (index < 0 || index >= getNbRecipes()) {
+            throw new IllegalArgumentException("Recipe index out of range");
+        }
+
+        recipes.remove(index);
+    }
+
+
+
+
+    /**
+     * Return the recipe at the given index.
+     *
+     * @param index
+     *      The index of the requested recipe.
+     *
+     * @pre The index of the requested recipe must be less than the total amount of recipes
+     *      | index < getNbRecipes()
+     *
+     * @return The recipe at the given index.
+     *       | result == recipes.get(index)
+     *
+     */
+    @Basic
+    public Recipe getRecipeAt(int index) {
+        return recipes.get(index);
+    }
+
+    /**
+     * Return the number of recipes in this recipe book.
+     *
+     * @return The number of recipes in this recipe book.
+     *       | result == recipes.size()
+     */
+    @Basic
+    public int getNbRecipes() {
+        return recipes.size();
+    }
 }
--- src/main/java/alchemy/recipes/RecipeStep.java
+++ src/main/java/alchemy/recipes/RecipeStep.java
@@ -1,4 +1,5 @@
 package alchemy.recipes;
 
 public class RecipeStep {
+    // TODO, in opgave staat er met 3 lijsten ma da ga nie met RecipeStep
 }
